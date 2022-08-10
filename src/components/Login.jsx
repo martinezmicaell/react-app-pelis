7d63c72cbd5c39c9748a91ba8d9babc4a6974b1c
Micael-Martinez <programacionmicael@gmail.com>
2022-08-10 17:22:10 -0300

    Puliendo la View Login2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import catImage from "../images/cat-running.webp";
+
+console.log(catImage);
+const MySwal = withReactContent(Swal);
 
 const Login = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-
 	const validateEmail = (email) => {
 		return String(email)
 			.toLowerCase()
@@ -12,32 +16,69 @@ const Login = () => {
 			);
 	};
 
-	const isEmail = validateEmail(email);
-
+	//Handles
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
+		const email = e.target.email.value;
+		const password = e.target.password.value;
+
+		if (email === "" || password === "") {
+			MySwal.fire("Los campos no pueden estar vacios!");
+			return;
+		}
+
+		if (email !== "" && !validateEmail(email)) {
+			console.log("Debes escribir una direccion de Email valida!");
+			return;
+		}
+
+		if (email !== "challenge@alkemy.org" || password !== "react") {
+			MySwal.fire("Credenciales Invalidas!");
+			return;
+		}
+
+		console.log({ email, password }, "Credenciales enviadas al back");
+
+		axios.post("http://challenge-react.alkemy.org", { email, password }).then((res) => {
+			MySwal.fire({
+				icon: "success",
+				title: "Tu gatito ha sido posteado correctamente!",
+				width: 500,
+				padding: "3em",
+				color: "#716add",
+				background: `url(/static/media/cat-running.dee526226c5cb884d75e.webp)
+                    center center
+                    no-repeat`,
+				backdrop: `
+                  rgba(0,0,123,0.4)
+                `,
+			});
+			console.log(res.data);
+		});
+
+		//resetear el form al postear la data
+		e.target.reset();
 	};
 
 	const handleEmail = (e) => {
-		setEmail(e.target.value);
+		// setEmail(e.target.value);
 	};
 
 	const handlePassword = (e) => {
-		setPassword(e.target.value);
+		// setPassword(e.target.value);
 	};
 
 	return (
 		<>
 			<h2>Formulario de Login</h2>
 			<form onSubmit={handleSubmit}>
-				<input type="email" name="email" placeholder="Email" onChange={handleEmail} value={email} />
-				<input type="password" name="password" placeholder="Password" onChange={handlePassword} value={password} />
+				<input type="email" name="email" placeholder="Email" />
+				<input type="password" name="password" placeholder="Password" />
 
-				<div>{isEmail ? <p>Felicidades, escribiste un mail!</p> : null} </div>
+				{/* <div>{isEmail ? <p>Felicidades, escribiste un mail!</p> : null} </div> */}
 
-				<button disabled={!isEmail} type="submit">
-					Ingresar
-				</button>
+				<button type="submit">Ingresar</button>
 			</form>
 		</>
 	);
